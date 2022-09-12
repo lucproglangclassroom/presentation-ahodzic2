231b112f8eb23ebb9645223486d954e66c0b04df
ahodzic2 <72961430+ahodzic2@users.noreply.github.com>
2022-09-12 18:57:04 -0500

    Added presentation and sample code

---
+++ LightningTalkDemo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace LightningTalkDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //User Input
+            int num;
+            Console.Write("Enter a number: ");
+            num = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("{0} is {1}", num, OddOrEven(num));
+            Console.ReadKey();
+        }
+
+        static string OddOrEven(int number)
+        {
+            if (number % 2 == 0)
+            {
+                return "even";
+            }
+            else
+            {
+                return "odd";
+            }
+        }
+    }
+}
